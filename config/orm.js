ecd2a1ca8f3591d81c3a076f89bfecfdd3aee176
rparker24 <robert@robertmparker.net>
2016-05-09 21:54:59 -0400

    file structure and initial setup

---
+++ config/orm.js
@@ -0,0 +1,34 @@
+var connection = require('../config/connection.js');
+
+var orm = {
+  all: function( , cb) {
+    var queryString = // mysql queryString
+    console.log(queryString);
+    connection.query(queryString, function() {
+
+    });
+  },
+  create: function( , cb) {
+    var queryString = // mysql queryString
+    console.log(queryString);
+    connection.query(queryString, function() {
+
+    });
+  },
+  update: function( , cb) {
+    var queryString = // mysql queryString
+    console.log(queryString);
+    connection.query(queryString, function() {
+
+    });
+  },
+  delete: function( , cb) {
+    var queryString = // mysql queryString
+    console.log(queryString);
+    connection.query(queryString, function() {
+
+    });
+  }
+};
+
+modules.export = orm;
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+var cat = require('../models/burger.js');
+
+
+modules.export = router;
