6d27c8028e8d48d6a906aff7692fd40b2913d0fc
rparker24 <robert@robertmparker.net>
2016-05-13 00:53:58 -0400

    add default to date column for schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,6 @@ USE burgers_db;
 CREATE TABLE burgers (id int NOT NULL AUTO_INCREMENT,
    burger_name VARCHAR(40) NOT NULL,
    devoured BOOLEAN DEFAULT 0,
-   createdDate TIMESTAMP NOT NULL,
+   createdDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
    PRIMARY KEY(id)
  );
