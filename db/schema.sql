f65425711c522c4e85fc30be9912c2db6070751a
rparker24 <robert@robertmparker.net>
2016-05-13 00:41:23 -0400

    update mysql schema formatting

--- db/schema.sql
+++ db/schema.sql
@@ -2,4 +2,9 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
-CREATE TABLE burgers (id int NOT NULL AUTO_INCREMENT, burger_name VARCHAR(40) NOT NULL, devoured BOOLEAN DEFAULT 0, date TIMESTAMP NOT NULL, PRIMARY KEY(id));
+CREATE TABLE burgers (id int NOT NULL AUTO_INCREMENT,
+   burger_name VARCHAR(40) NOT NULL,
+   devoured BOOLEAN DEFAULT 0,
+   createdDate TIMESTAMP NOT NULL,
+   PRIMARY KEY(id)
+ );
